dc7c9048abdb0676884a1274c54d10c00f0ee63f
chiquetaMariano <mariano_cr90@hotmail.com>
2021-11-15 21:11:35 -0300

    Remove unnecessary /bin folder and files
    Port normalization is going to be refactored together with app.js file.
    We are going to use loaders to setup app initialization.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+var config = require('./config');
 var express = require('express');
 var path = require('path');
+
+var http = require('http');
+var debug = require('debug')('express-typescript:server');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -15,4 +20,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-module.exports = app;
+app.set('port', config.port);
+
+// START SERVER
+var server = http.createServer(app);
+server.listen(config.port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+// module.exports = app;
